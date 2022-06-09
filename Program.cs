bf2a83e7e1b729430b0577154f7f6dbe8f15a037
Nathan Soares <89109120+Nathanscs@users.noreply.github.com>
2022-06-09 18:53:16 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Ex9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] aleatorio;
+            int[,] aleatorios;
+            int i, j;
+            aleatorio = new int[10,10];
+            aleatorios = new int[10,10];
+
+            for(i=0;i<10;i++)
+            {
+                for(j=0;j<10;j++)
+                {
+                    Random rnd = new Random();
+                    aleatorio[i,j] = rnd.Next(0, 10);
+                }
+            }
+            for(i=0;i<10;i++)
+            {
+                for(j=0;j<10;j++)
+                {
+                    Random rnd = new Random();
+                    aleatorios[i,j] = rnd.Next(0, 10);
+                }
+            }
+            for(i=0;i<10;i++)
+            {
+                for(j=0;j<10;j++)
+                {
+                    if(aleatorio[i,j]==aleatorios[i,j])
+                    {
+                        Console.WriteLine("[{0}][{1}] é igual!          {2} e {3}", i, j, aleatorio[i,j], aleatorios[i,j]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("[{0}][{1}] não é igual!      {2} e {3}", i, j, aleatorio[i,j], aleatorios[i,j]);
+                    }
+                }
+            }
+        }
+    }   
+}
+
